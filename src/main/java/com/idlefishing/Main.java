5eb5d04c4ad1b802dfdfdf20167f7a839b9b8e40
CupOfTea12 <orioncatspaceradarsr@gmail.com>
2026-05-18 23:01:21 +0200

    added a lovely comment

--- src/main/java/com/idlefishing/Main.java
+++ src/main/java/com/idlefishing/Main.java
@@ -1,9 +1,11 @@
 package com.idlefishing;
 /*
 This gem written by me on 2 hours of sleep and a can of redbull is something between a
-cookie clicker game and fisherman dream
+cookie clicker game and fisherman dream (i had this game in my sleeve for a while so it was not done from a scratch, i had some existing parts of it before)
 
 All by me (CupOfTea12 on Github)
+I hope that anyone who opnes up this code will like it and understand it like i do
+:)
  */
 import com.idlefishing.ui.MainFrame;
 import javax.swing.SwingUtilities;
